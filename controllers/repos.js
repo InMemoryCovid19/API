c876fe1d47db3dfaf7f417656df4b0994ab82a3f
LesliePajuelo <leslie.pajuelo@hackreactor.com>
2020-04-05 05:46:46 -0700

    woring create repo

--- controllers/repos.js
+++ controllers/repos.js
@@ -59,8 +59,19 @@ exports.createRepo = async function () {
 const config = require('../config');
 const ghGot = require('gh-got');
 
-const repoInfo = async function() {
+const reposInfo = async function() {
     const url = "/orgs/InMemoryCovid19/repos"
+	try{
+    const response = await ghGot(url, {token:config.github_token });
+    console.log('repo', response.body)
+	return response.body;
+	} catch (error){
+		console.log('repoInfo', error)
+	}
+};
+
+const repoInfo = async function() {
+    const url = "/repos/InMemoryCovid19/StartHere"
 	try{
     const response = await ghGot(url, {token:config.github_token });
     console.log('repo', response.body)
@@ -73,28 +84,33 @@ const repoInfo = async function() {
 exports.createRepo = async function(){
     //repos/:template_owner/:template_repo/generate
     // const url = "/repos/InMemoryCovid19/StartHere/generate";
-
+// const body = await repoInfo();
     //GET /repos/:owner/:repo
-    const url = "/repos/InMemoryCovid19/StartHere/";
+    // const url = "/repos/InMemoryCovid19/StartHere";
+
+    const url = "/orgs/InMemoryCovid19/repos";
 
     try{
-        var response = await ghGot(url, {
+        var response = await ghGot.post(url, {
             headers: {
                 Accept:'application/vnd.github.baptiste-preview+json'
             },
-            json: {
-                "owner": "InMemoryCovid19",
-                "name": "Test",
+            body: {
+                "name": "Hello-World",
                 "description": "This is your first repository",
-                "private": false
+                "homepage": "",
+                "private": false,
+                "has_issues": false,
+                "has_projects": false,
+                "has_wiki": true
               },
             responseType: 'json',
             
             token:config.github_token });
-        console.log(response.body);
+        return {name: response.body.name, }
         } catch(error){
             console.log('createRepo', error);
-            console.log('createRepo', response);
+            console.log('createRepo.response', error.response.body.errors);
         }
 };
 >>>>>>> wip repo creation
